f2770a10ed4d33930c8a263dfc1d764e012073d9
mikerspencer <spencer.mike.r@gmail.com>
2021-03-08 15:45:31 +0000

    etl now filters by latest data

--- etl_hnc.R
+++ etl_hnc.R
@@ -13,6 +13,9 @@ library(readxl)
 
 # read data ---------------------------------------------------------------
 
+# 0 in NA to account for mandatory fields.
+# 0 should be dropped when subsequent data come in.
+
 df = read_excel("Placement Provision.xlsx", na = c("-99", "0")) %>%
   janitor::clean_names()
 
@@ -37,37 +40,48 @@ college_xy = college_postcode %>%
 # clean -------------------------------------------------------------------
 
 x = df %>%
-  select(-completion_time, -email, -name, -your_name) %>%
+  select(-id, -completion_time, -email, -name, -your_name) %>%
   rename(centre_name = which_college_are_you_responding_for) %>%
   left_join(college_xy)
 
 x_enrol = x %>%
-  select(id, contains("enrolled")) %>%
-  pivot_longer(!id, values_to = "enrolled") %>%
+  select(centre_name, start_time, contains("enrolled")) %>%
+  pivot_longer(contains("enrolled"), values_to = "enrolled") %>%
+  drop_na(enrolled) %>%
+  group_by(centre_name, name) %>%
+  filter(start_time == max(start_time)) %>%
+  ungroup() %>%
   mutate(name = str_remove(name, "how_many_"),
          name = str_remove(name, "_students_are_enrolled")) %>%
-  separate(name, c("course", "option"), sep = "_on_") %>%
-  drop_na(enrolled)
+  separate(name, c("course", "option"), sep = "_on_")
+
 
 x_place = x %>%
-  select(id, contains("placement")) %>%
-  pivot_longer(!id, names_to = "course", values_to = "not_placed") %>%
+  select(centre_name, start_time, contains("placement")) %>%
+  pivot_longer(contains("placement"), names_to = "course", values_to = "not_placed") %>%
+  drop_na(not_placed) %>%
+  group_by(centre_name, course) %>%
+  filter(start_time == max(start_time)) %>%
+  ungroup() %>%
   mutate(course = str_remove(course, "how_many_"),
          course = str_remove(course, "_students_do_not_have_a_placement"),
-         course = str_remove(course, "_option_2")) %>%
-  drop_na(not_placed)
+         course = str_remove(course, "_option_2"))
 
 df_clean = x %>%
-  select(id, start_time, centre_name, easting, northing) %>%
+  select(centre_name, start_time, easting, northing) %>%
   full_join(x_enrol) %>%
   full_join(x_place) %>%
-  mutate(not_placed = replace(not_placed,
-                              option %in% c("option_1",
-                                            "option_3",
-                                            "theoretical_group_award"),
+  mutate(placement_req = if_else(option %in% c("option_1",
+                                               "option_3",
+                                               "theoretical_group_award"),
+                                 "no", "yes"),
+         not_placed = replace(not_placed,
+                              placement_req == "no",
                               NA),
          enrolled = as.numeric(enrolled),
-         not_placed = as.numeric(not_placed))
+         not_placed = as.numeric(not_placed),
+         not_placed = if_else(is.na(not_placed) & placement_req == "yes",
+                              enrolled, not_placed))
 
 
 # QA ----------------------------------------------------------------------
